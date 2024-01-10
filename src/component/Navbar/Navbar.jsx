d58077b6c8c792001c3bf4202a105084d4a54817
lenovo <black03139090313@gmail.com>
2024-01-10 20:36:21 +0500

    NAVBAR

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
-
+import Logo from '../../assets/Logo.svg'
+import "../../App.css"
+import "./navbar.css"
+import Search from "../../assets/SearchBlack.svg"
 function Navbar(props) {
     return (
-        <div>Navbar Page</div>
+        <div className="container mt-2 mb-3">
+            <div className="navbar">
+                <img src={Logo} alt=""/>
+                <div className="sections">
+                    <a href="Main">Asosiy</a>
+                    <a href="Section">Bo'limlar</a>
+                    <a href="Autor">Muallifning-qarashlari</a>
+                </div>
+                <div className="search d-flex">
+                    <img src={Search} alt=""/>
+                    <input type="text" placeholder="Qidirish"/>
+                </div>
+            </div>
+        </div>
     );
 }
 
