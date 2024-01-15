462ab2a6f18ad2ae7052ee8bfa144dac245b99cd
JovliyevOlim <olimjovliyev98@gmail.com>
2024-01-15 10:47:56 +0500

    logo

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Logo from '../../assets/Logo.svg'
+import Logo2 from '../../assets/UNDPlogo 2.svg'
 import "../../App.css"
 import "./navbar.css"
 import Search from "../../assets/SearchBlack.svg"
@@ -28,14 +29,14 @@ function Navbar(props) {
                         <div className="col-12 col-md-12 col-lg-3">
                             <img className={'navbar-logo'} src={Logo} alt=""/>
                         </div>
-                        <div className="col-12 col-md-6 col-lg-5">
+                        <div className="col-12 col-md-5 col-lg-5">
                             <div className="navbar-items">
                                 <NavHashLink className={`${location.pathname}${location.hash}` === "/#home" ? "navbar-item-active" : "navbar-item"}  to="/#home">Асосий</NavHashLink>
                                 <NavHashLink  className={`${location.pathname}${location.hash}` === "/#category" ? "navbar-item-active" : "navbar-item"} to="/#category">Бўлимлар</NavHashLink>
                                 <NavHashLink  className={`${location.pathname}${location.hash}` === "/author" ? "navbar-item-active" : "navbar-item"} to="/author">Муаллифнинг қарашлари</NavHashLink>
                             </div>
                         </div>
-                        <div className="col-12 col-md-6 col-lg-3">
+                        <div className="col-12 col-md-5 col-lg-3">
                             <div className='navbar-right'>
                                 <div className="navbar-search">
                                     <div className={'navbar-input'}>
@@ -48,6 +49,10 @@ function Navbar(props) {
                                     }
                                 </div>
                             </div>
+                        </div>
+                        <div className="col-12 col-md-2 pt-4 pt-md-1 col-lg-1 d-flex align-items-center">
+                            <img className={'navbar-logo2'} src={Logo2} alt="logo2"/>
+
                         </div>
                     </div>
                 </div>
